0d3b2918bf9d5b49f2695e26ec5d7d2989013788
Josiah Vinson <jovinson@microsoft.com>
2020-08-06 18:01:50 -0700

    Introduced startup and DI

---
+++ EventHubDI/EventHubFunction/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(EventHubFunction.Startup))]
+
+namespace EventHubFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            Console.WriteLine(builder.Services.Count);
+        }
+    }
+}
